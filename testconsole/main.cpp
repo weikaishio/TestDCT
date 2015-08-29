f526fd8ee3e492eab90ca7ac017c41f2e4874176
weikaishio <weikaishio@163.com>
2015-08-29 16:06:45 +0800

    add read ini

--- testconsole/main.cpp
+++ testconsole/main.cpp
@@ -13,6 +13,12 @@
 #include <vector>
 
 #include "ziphelper.h"
+#include "miniini.h"
+
+struct ConfigModel{
+    int fileType;
+    string fileName;
+};
 
 //void UnzipAndZip( const char* pScrFileName, const char* pDstFileName )
 //{
@@ -64,7 +70,37 @@
 //    }
 //    zipClose( ZipFile, NULL );
 //}
-
+bool readIni(string filePath,ConfigModel &model){
+    INIFile ini;
+    if(!ini.OpenFile(filePath)){
+        std::cout << "ERROR: Could not open ini" << std::endl;
+        return false;
+    }
+    
+    std::string sname = "xxx";
+    INISection * general = ini.GetSection(sname);
+    if(!general){
+        std::cout << "ERROR: Missing section [general] in ini"
+        << std::endl;
+        return false;
+    }
+    
+    std::string filename;
+    int filetype;
+    std::string filename_tag = "filename";
+    std::string filetype_tag = "filetype";
+    if(general->ReadString(filename_tag, filename)){
+        std::cout << "section [" << filename
+        << "] in ini" << std::endl;
+        model.fileName=filename;
+    }
+    if(general->ReadInt(filetype_tag, filetype)){
+        std::cout << "section [" << filetype
+        <<"] in ini" << std::endl;
+    model.fileType=filetype;
+    }
+    return true;
+}
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
@@ -74,6 +110,7 @@ int main(int argc, const char * argv[]) {
     unzipStatus result=ziphelper::unzip("/Users/qihoo/cpp/Test/TestConsole/TestCPP/testconsole/xx.zip", "/Users/qihoo/cpp/Test/TestConsole/TestCPP/testconsole", true, "123456",files);
     std::cout<<"ziphelper::unzip:"<<result<<std::endl;
     
+    string configFile="config.ini";
     vector<string>::iterator viter;
     for(viter = files.begin(); viter != files.end(); viter++)
     {
@@ -81,8 +118,12 @@ int main(int argc, const char * argv[]) {
         string fullPath=*viter;
         int index=fullPath.find_last_of('/');
         string pureFileName=fullPath.substr(index+1,fullPath.size()-index-1);
-        if(pureFileName==string("config.ini")){
+        if(pureFileName==configFile){
             cout<<"config:"<<fullPath<<endl;
+            ConfigModel model;
+            if (readIni(fullPath, model)) {
+                cout<<"ConfigModel:"<<model.fileType<<","<<model.fileName<<endl;
+            }
         }
         
     }
